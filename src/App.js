bcb45ab430c9dd0cfb532dc2de6d2ef16b4c3f90
Vaux Gomes <vauxsandino@gmail.com>
2021-09-18 23:46:17 -0300

    Better look and feel when in landscape mode

--- src/App.js
+++ src/App.js
@@ -24,27 +24,26 @@ function App() {
         <Banner />
 
         {/* MAIN */}
-        <div>
-          <main className="ideias">
-            <div className="search-bar">
-              <span className="icon-lg material-icons text-muted">search</span>
-              <input
-                id="search"
-                type="text"
-                placeholder="Procure por uma idéia"
-              />
-            </div>
 
-            <IdeaForm />
+        <main className="ideias pb-5">
+          <div className="search-bar">
+            <span className="icon-lg material-icons text-muted">search</span>
+            <input
+              id="search"
+              type="text"
+              placeholder="Procure por uma idéia"
+            />
+          </div>
 
-            <h6 className="fw-light">Mais avaliados...</h6>
-            <div className="cards">
-              {ideas.map((idea, key) => (
-                <Card idea={idea} key={key} />
-              ))}
-            </div>
-          </main>
-        </div>
+          <IdeaForm />
+
+          <h6 className="fw-light">Mais avaliados...</h6>
+          <div className="cards">
+            {ideas.map((idea, key) => (
+              <Card idea={idea} key={key} />
+            ))}
+          </div>
+        </main>
       </div>
     </Router>
   )
